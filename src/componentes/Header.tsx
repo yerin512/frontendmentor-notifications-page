804f69b0286989ba9deecfd09e1823de5957924c
yerin512 <sosu0512@naver.com>
2023-03-05 01:42:11 +0900

    Mark all as read 버튼을 누르면 모든 알림을 제거하라

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -1,15 +1,29 @@
-interface NotificationNumberType {
-  notificationNumber: number
-}
+import { useAtom } from 'jotai'
+import { notificationsAtom } from 'atoms/notifications';
 
-export default function Header({ notificationNumber }: NotificationNumberType) {
+export default function Header() {
+  const [notifiactions, setNotifications] = useAtom(notificationsAtom);
+
+  const notificationNumber = notifiactions.filter(({ newNotification }) => newNotification).length;
+
+  const removeNewNotification = () => notifiactions.map(notification => ({ ...notification, newNotification: false }))
+
+  const handleClickAllRead = () => {
+    setNotifications(removeNewNotification);
+  }
+
   return (
     <div className='notifications__header'>
       <div className='notifications__title-wrap'>
         <h1 className='notifications__title'>Notifications</h1>
         <span className='notifications__number'>{notificationNumber}</span>
       </div>
-      <p className='notification__confirm-button'>Mark all as read</p>
+      <button
+        className='notification__confirm-button'
+        onClick={handleClickAllRead}
+      >
+        Mark all as read
+      </button>
     </div>
   )
 }
