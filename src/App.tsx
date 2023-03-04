7680ed130bfb67ffa6f61e2f5baa386dbe95282b
yerin512 <sosu0512@naver.com>
2023-03-04 13:41:48 +0900

    프로젝틑에 TypeScript를 적용하라 프로젝트에 typescript를 적용하여 index와 App 파일의 확장자를 tsx로 변경했습니다.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from 'componentes/Header';
+import Notifications from 'componentes/Notifications';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Notifications />
+    </div>
+  );
+}
+
+export default App;
